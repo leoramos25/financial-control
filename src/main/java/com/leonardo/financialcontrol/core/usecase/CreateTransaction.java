ea883995082882a7a4d5ff4c676d52fe2eaedc3b
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-09-30 18:33:07 -0300

    feature: command pattern

--- src/main/java/com/leonardo/financialcontrol/core/usecase/CreateTransaction.java
+++
@@ -1,7 +0,0 @@
-package com.leonardo.financialcontrol.core.usecase;
-
-import com.leonardo.financialcontrol.core.domain.Transaction;
-
-public interface CreateTransaction {
-  Transaction execute(Transaction transaction);
-}
