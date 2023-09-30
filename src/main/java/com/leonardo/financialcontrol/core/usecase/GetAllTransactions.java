47b2148cefc38577b8293402c5f63d06448df3ee
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-09-30 18:38:46 -0300

    fix: delete get all trasanctions interface

--- src/main/java/com/leonardo/financialcontrol/core/usecase/GetAllTransactions.java
+++
@@ -1,8 +0,0 @@
-package com.leonardo.financialcontrol.core.usecase;
-
-import com.leonardo.financialcontrol.core.domain.Transaction;
-import java.util.List;
-
-public interface GetAllTransactions {
-  List<Transaction> execute();
-}
